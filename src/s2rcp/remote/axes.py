0c0c5d86836a1f3cf879b021f91d4700e93c2322
Kirill Zhikharev <zhikh.k@gmail.com>
2023-04-06 19:40:43 +0300

    Fix a bug in AxesConfig

--- src/s2rcp/remote/axes.py
+++ src/s2rcp/remote/axes.py
@@ -7,15 +7,15 @@ class AxesConfig:
         self._axes = dict()
 
     def setup_from_json(self, json_str):
-        self._axes_config = json.loads(json_str)
+        self._axes = json.loads(json_str)
 
     def add_new_axis(self, axis_name):
-        self._axes_config[axis_name] = dict()
+        self._axes[axis_name] = dict()
 
     def add_motor_to_axis(self, axis_name, motor_id, motor_k):
         if axis_name not in self._axes.keys():
             self.add_new_axis(axis_name)
-        self._axes_config[axis_name][motor_id] = motor_k
+        self._axes[axis_name][motor_id] = motor_k
 
     def get_all_axes(self):
         return list(self._axes.keys())
