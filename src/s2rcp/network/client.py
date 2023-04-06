001d26ce00b00826a5d3fb5f9cdb49f9b5c3cbb4
Kirill Zhikharev <zhikh.k@gmail.com>
2023-04-06 17:59:44 +0300

    Fix a bug with class Client

--- src/s2rcp/network/client.py
+++ src/s2rcp/network/client.py
@@ -11,7 +11,7 @@ class ClientEvent(Enum):
 
 class BaseClient(metaclass=ABCMeta):
     def __init__(self):
-        self.__handlers = dict()
+        self._handlers = dict()
 
     @abstractmethod
     def send(self, data) -> None:
@@ -22,18 +22,18 @@ class BaseClient(metaclass=ABCMeta):
         Subscribes a function to a specific event.
         When calling a function, arguments are passed as a dictionary
         """
-        if event_type not in self.__handlers.keys():
-            self.__handlers[event_type] = list()
-        self.__handlers[event_type].append(handler)
+        if event_type not in self._handlers.keys():
+            self._handlers[event_type] = list()
+        self._handlers[event_type].append(handler)
 
     def _call_handlers(self, event_type, *args):
         """
         Calls all handlers subscribed to this event
         When calling a handler, arguments are passed as a dictionary
         """
-        if event_type not in self.__handlers.keys():
+        if event_type not in self._handlers.keys():
             return
 
-        for handler in self.__handlers[event_type]:
+        for handler in self._handlers[event_type]:
             Thread(target=handler, args=args, daemon=True).start() 
 
